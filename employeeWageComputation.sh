355d0b55d945e281a62b3f16a23986b7b9c55949
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-10 16:28:33 +0530

    REFACTOR : Added Attendance Code to File employeeWageComputation.sh

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash -x
 
 echo "Welcome To employee Wage Computation"
+
+attendance=$(( RANDOM%2 ))
+
+if [ $attendance -eq 0 ]
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Absent"
+fi
