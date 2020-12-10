381cdf372afe123ce81a8a21a3fd10f1ff31fcfa
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-10 17:08:13 +0530

    REFACTOR : added a daily salary calculation Code in employeeWageComputation.sh

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash -x
 
+wagePerHour=20
+hourInADay=8
+salary=0
+
 echo "Welcome To employee Wage Computation"
 
 attendance=$(( RANDOM%2 ))
 
 if [ $attendance -eq 0 ]
 then
-        echo "Employee is Present"
+        salary=$(( $hourInADay*$hourInADay ))
 else
-        echo "Employee is Absent"
+        salary=0
 fi
+
+echo "Wage of one day : " $salary
